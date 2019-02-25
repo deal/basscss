39e90e2d6977ff1708fbb06c996727712d258367
Kyle Sosnowski <ktsosno@gmail.com>
2019-02-25 14:52:14 -0800

    feat: add global variables

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,18 @@
+const path = require('path')
+
 module.exports = {
   plugins: [
-    "postcss-import",
-    "postcss-custom-media",
-    "postcss-custom-properties",
-    "postcss-calc",
-    "cssstats",
-    "postcss-discard-comments",
-    "postcss-remove-root",
-    "autoprefixer",
-    "postcss-reporter"
-  ].map(require)
-};
+    require('postcss-import')(),
+    require('postcss-custom-media')(),
+    require('postcss-calc')(),
+    require('postcss-reporter')(),
+    require('postcss-custom-properties')(),
+    require('postcss-discard-comments')(),
+    require('postcss-remove-root')(),
+    require('autoprefixer')(),
+    require('postcss-preset-env')({
+      // default is stage 2, but we need this due to nesting @media queries inside classes
+      stage: 0
+    })
+  ]
+}
